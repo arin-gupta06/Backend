ede225a16f0ecdcd3fa767454085f8a90c657792
arin-gupta06 <24cd10ar15@mitsgwl.ac.in>
2026-05-16 13:33:57 +0530

    feat: set up Express server with MongoDB connection and error handling
    - Updated package.json to include necessary dependencies and modified dev script.
    - Created app.js to initialize Express with middleware for CORS, JSON parsing, and cookie parsing.
    - Added constants.js for database name configuration.
    - Implemented db.js for MongoDB connection logic using Mongoose.
    - Set up index.js to configure environment variables, connect to the database, and start the server.
    - Introduced utility classes for API error and response handling.
    - Added asyncHandler utility for managing asynchronous route handlers.

---
+++ VideoTube/src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (fn) => (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//  const asyncHanlder = (reqHandler) => {
+//     (req, res, next) => {
+//         Promise.resolve(reqHandler(req, res, next)).catch((err) => next(err))
+//     }
+//  }
+
+export {asyncHandler}
